27c4a133f1e78c9938177ac023d64b0b3c9bc030
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-09-21 17:12:06 -0400

    locaiton converter and weather api function built

--- api.py
+++ api.py
@@ -10,22 +10,23 @@ ctx = ssl.create_default_context(cafile=certifi.where())
 geopy.geocoders.options.default_ssl_context = ctx
 
 
-geolocator = Nominatim(user_agent="my_request")
-getLoc = geolocator.geocode("Plattsburgh")
+def weather_specs(location: str) -> tuple[str, str, str]:
+    """Function that takes in any location on earth and (address, city, state)
+    and returns a tuple with the attributes, in order, of the precipitation 
+    probability, the cloud cover and the visibility of the input location at the
+    current time, even optimizing for timezones"""
+    geolocator = Nominatim(user_agent="my_request")
+    getLoc = geolocator.geocode(location)
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={getLoc.latitude}&longitude={getLoc.longitude}&hourly=precipitation_probability,cloudcover,visibility&timezone=auto&forecast_days=3")
 
-print(getLoc.latitude, getLoc.longitude)
+    # making sure the API key was input correctly
+    if response.status_code != 200:
+        print("API Key Invalid")
+        return None
+    
+    curr_cloud = response.json()["hourly"]["cloudcover"][dt.datetime.now().hour]
+    curr_visibility = response.json()["hourly"]["visibility"][dt.datetime.now().hour]
+    curr_precipitation_prob = response.json()["hourly"]["precipitation_probability"][dt.datetime.now().hour]
+    return (curr_precipitation_prob, curr_cloud, curr_visibility)
 
-response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={getLoc.latitude}&longitude={getLoc.longitude}&hourly=precipitation_probability,cloudcover,visibility&timezone=auto&forecast_days=3")
-
-print(f"https://api.open-meteo.com/v1/forecast?latitude={getLoc.latitude}&longitude={getLoc.longitude}&hourly=precipitation_probability,cloudcover,visibility&timezone=auto&forecast_days=3")
-print(dt.datetime.now().hour)
-
-if response.status_code != 200:
-    print("API Key Invalid")
-
-
-current_cloud = response.json()["hourly"]["cloudcover"][dt.datetime.now().hour]
-current_visibility = response.json()["hourly"]["visibility"][dt.datetime.now().hour]
-
-print("The current total cloud cover is " + str(current_cloud) + "%")
-print("The current visibility is " + str(current_visibility) + "meters")
+print(weather_specs("Toronto"))
