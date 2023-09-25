4fb31b5ff541059d80d287b2ae56bbdca20ee91d
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-09-25 11:24:08 -0400

    astroquery added, added dictionaries

--- api.py
+++ api.py
@@ -4,10 +4,17 @@ import ssl
 import certifi
 import geopy.geocoders
 from geopy.geocoders import Nominatim
+from astroquery.simbad import Simbad
+from astropy.coordinates import SkyCoord
 
-# ssl certification for geopy
+
+
+
+
+# ssl certification for geopy and VOT mods for SIMBAD
 ctx = ssl.create_default_context(cafile=certifi.where())
 geopy.geocoders.options.default_ssl_context = ctx
+Simbad.add_votable_fields('flux(V)', 'distance')
 
 
 def weather_specs(location: str) -> tuple[str, str, str]:
@@ -24,9 +31,24 @@ def weather_specs(location: str) -> tuple[str, str, str]:
         print("API Key Invalid")
         return None
     
-    curr_cloud = response.json()["hourly"]["cloudcover"][dt.datetime.now().hour]
-    curr_visibility = response.json()["hourly"]["visibility"][dt.datetime.now().hour]
-    curr_precipitation_prob = response.json()["hourly"]["precipitation_probability"][dt.datetime.now().hour]
-    return (curr_precipitation_prob, curr_cloud, curr_visibility)
+    weather_list = {
+        "address": str(getLoc.address) + "(" + str(round((getLoc.longitude), 3)) + ", " + str(round((getLoc.latitude), 3)) + ")",
+        "curr_cloud": str(response.json()["hourly"]["cloudcover"][dt.datetime.now().hour]) + "%",
+        "curr_visibility": str(response.json()["hourly"]["visibility"][dt.datetime.now().hour]) + " meters",
+        "curr_precipitation_prob": str(response.json()["hourly"]["precipitation_probability"][dt.datetime.now().hour]) + "%"
+    }
+    return weather_list
 
-print(weather_specs("Toronto"))
+def astro_object(obj: str) -> dict:
+    result_table = Simbad.query_object(str(obj))
+    if result_table is None: return "The inputed object was not recognized by the SIMBAD Astronomical Database"
+
+    obj_list = {
+        "name": result_table[0][0],
+        "distance": str(result_table[0][-8]) + result_table[0][-6],
+        "apparent magnitude": result_table[0][-9],
+        "wikipedia": f"wikipedia.org/wiki/{obj.replace(' ', '_')}"
+    }
+    return obj_list
+
+print(weather_specs("mount marcy"))
