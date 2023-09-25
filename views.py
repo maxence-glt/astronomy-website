a35ee411810480596b42b80897fe87ad900b61d2
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-09-25 11:24:51 -0400

    changed routing to multiple pages

--- views.py
+++ views.py
@@ -1,16 +1,40 @@
 from flask import Blueprint, render_template, request
-from api import current_cloud, current_visibility
+from api import weather_specs, astro_object
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", location="Your location", cloud=current_cloud, visibility=current_visibility)
+    return render_template("index.html")
 
-@views.route('/data', methods = ['POST', 'GET'])
+@views.route("/", methods=['POST', 'GET'])
+def button():
+    if request.method == 'POST':
+        return render_template("index.html")
+
+@views.route("/astro-catalog", methods = ['POST', 'GET'])
+def astro():
+    if request.method == 'GET':
+        return f"The URL /astro-catalog is accessed directly."
+    if request.method == 'POST':
+        obj = request.form.get("astronomy")
+        astro_list = astro_object(str(obj))
+        return render_template("catalog.html",
+    	                       input_name=obj,
+    	                        name=astro_list["name"],
+    	                          distance=astro_list["distance"],
+    	                            mag=astro_list["apparent magnitude"],
+    	                              wikipedia=astro_list["wikipedia"])
+
+@views.route('/weather', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return f"The URL /weather is accessed directly."
     if request.method == 'POST':
         location = request.form.get("location")
-        return render_template('index.html', cloud=current_cloud, visibility=current_visibility, location = location)
+        weather = weather_specs(str(location))
+        return render_template('weather.html',
+                                precipitation=weather["curr_precipitation_prob"],
+                                  cloud=weather["curr_cloud"],
+                                    visibility=weather["curr_visibility"],
+                                      location=location)
